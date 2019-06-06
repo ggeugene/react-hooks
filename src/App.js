e74da559dba527d680518b100901e5ae2635f87d
ggeugene <g.eugene.g@outlook.com>
2019-06-06 12:11:49 +0300

    conditional useEffect example.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import HookCounterOne from './components/HookCounterOne'
+import ClassCounterOne from './components/ClassCounterOne'
 
 function App() {
   return (
     <div className='App'>
+      {/* <ClassCounterOne /> */}
       <HookCounterOne />
     </div>
   )
